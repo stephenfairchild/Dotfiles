d291d965c099fb28daa9dafe992848d57aca13ab
Stephen Fairchild <stephen.fairchild@researchsquare.com>
2020-07-18 09:59:10 -0400

    clean env zsh file

--- home/.zsh/environment.zsh
+++ home/.zsh/environment.zsh
@@ -1,9 +1,9 @@
-export PATH="$PATH:$HOME/Android/Sdk/platform-tools"
+# System
 export PATH=$PATH:/usr/local/bin
-
 export TERM=${TERM:-xterm-256color}
 export LANG=en_US.UTF-8
 
+# FZF
 export FZF_DEFAULT_OPS="--extended"
 export FZF_DEFAULT_OPTS='
     --color fg:223,bg:-1,hl:65,fg+:142,bg+:-1,hl+:108
@@ -12,5 +12,14 @@ export FZF_DEFAULT_OPTS='
 export FZF_CTRL_R_OPTS="--inline-info --exact"
 command -v dircolors > /dev/null && eval "$(dircolors ~/.dircolors)"
 
+# NVM
 export NVM_DIR="$HOME/.config"
+
+# Go
+export GOPROXY="https://proxy.golang.org,direct"
+export GOSUMDB=off
+export GO111MODULE=on
+
+# Android
+export PATH="$PATH:$HOME/Android/Sdk/platform-tools"
 
