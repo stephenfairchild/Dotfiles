bd69e03d9b4ae93e386a8a8dcdff4ff503c30e70
Stephen Fairchild <sfairchild@maxdigital.com>
2022-09-19 13:03:38 -0400

    init.lua

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,1 +1,12 @@
-init.vim
+require('plugins')
+require('settings')
+require('mappings')
+
+require('telescope').load_extension('gh')
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "c", "lua", "rust", "javascript", "graphql", "typescript" },
+  highlight = {
+    enable = true,
+  },
+}
