334377b29fdee514a3487ee0b53cb8f0b2926768
Stephen Fairchild <sfairchild@maxdigital.com>
2022-11-14 13:58:25 -0500

    changes

--- home/.config/nvim/lua/telescope-settings.lua
+++ home/.config/nvim/lua/telescope-settings.lua
@@ -1,13 +1,1 @@
-require('telescope').load_extension('gh')
-require('telescope').load_extension('bookmarks')
-require('telescope').setup {
-  extensions = {
-    bookmarks = {
-      -- selected_browser = 'brave',
-      -- url_open_command = "xdg-open",
-      url_open_plugin = "vim_external",
-      full_path = false,
-      debug = true,
-    },
-  }
-}
+../../../.homesick/repos/Dotfiles/home/.config/nvim/lua/telescope-settings.lua
